c3886ce50bf11f69a2d8b7e70ef8d5db3187c635
n1ce4rt <nice.lol.1994@gmail.com>
2022-02-09 13:55:08 +0300

    first push

---
+++ src/components/successful_modal/successful_modal.tsx
@@ -0,0 +1,11 @@
+import style from './poster_modal.module.css'
+
+export const PosterModal = () => {
+
+
+
+    return (
+        <section className={style.poster_wrapper}>
+        </section>
+    )
+}
